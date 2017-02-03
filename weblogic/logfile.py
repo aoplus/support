25a9dd21894062035ee8163d5d61304c077c17de
OmPrakash Singh <omprakash.singh@nomura.com>
2017-02-03 12:50:51 +0000

    updated script '

--- weblogic/logfile.py
+++ weblogic/logfile.py
@@ -1,37 +1,33 @@
 from java.lang import Boolean
 from java.lang import System
 from java.lang import Integer
-username = System.getProperty("user","weblogic")
-password = System.getProperty("password","weblogic")
-adminHost = System.getProperty("adminHost","localhost")
-adminPort = System.getProperty("adminPort","7001")
-protocol = System.getProperty("protocol","t3")
-url = protocol+"://"+adminHost+":"+adminPort
-fileCount = Integer.getInteger("fileCount", 100)
-fileMinSize = Integer.getInteger("fileMinSize", 5120)
-fileName = System.getProperty("fileName","config\\mydomain\\myserver\\myserver.log")
-log4jEnabled = System.getProperty("log4jEnabled", "true")
-print "Connecting to " + url + " as [" + \
-  username + "," + password + "]"
-# Connect to the server
-connect(username,password,url)
-edit()
-startEdit()
-# set CMO to the server log config
-cd("Servers/myserver/Log/myserver")
-ls ()
-print "Original FileCount is " + 'get("FileCount")'
-print "Setting FileCount to be " + \QfileCount\Q
-set("FileCount", fileCount)
-print "Original FileMinSize is " + 'get("FileMinSize")'
-print "Setting FileMinSize to be " + 'fileMinSize'
-set("FileMinSize", fileMinSize)
-print "Original Log4jEnabled is " + 'get("Log4jLoggingEnabled")'
-print "Setting Log4jLoggingEnabled to be " + 'log4jEnabled'
-set("Log4jLoggingEnabled", log4jEnabled)
-save()
-activate()
-print
-ls ()
+
+execfile('jython/connect.py')
+
+
+domainConfig()
+servers=cmo.getServers()
+
+for x in servers:
+    try:
+        edit()
+        startedit()
+        serverName = x.getName()
+        print '======= Enabling Log Retention for ' + serverName + '======='
+        cd('/Servers/'+serverName+'/WebServer/'+serverName+'/WebServerLog/'+serverName)
+        cmo.setLog4jLoggingEnabled(true)
+        cmo.setFileCount(100)
+        cmo.FileMinSize(5000)
+        save()
+        activate()
+        print ' '
+        print ' '
+        ls()
+    except:
+        java.lang.Exception, ex:
+        print 'Exception on Changing the Log Attributes: ' + ex.toString()
+        save()
+
 # all done...
 exit()
+
