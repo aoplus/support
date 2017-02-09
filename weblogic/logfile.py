1270e802a78600e9bc1c029e374ac29c0a6578fb
Om prakash singh <omps@omps.in>
2017-02-09 16:05:21 +0530

    added function to handle request

--- weblogic/logfile.py
+++ weblogic/logfile.py
@@ -2,36 +2,46 @@ from java.lang import Boolean
 from java.lang import System
 from java.lang import Integer
 
-execfile('jython/connect.py')
+execfile('jython/connect.py')  #String to connect to the weblogic server.
 
 
 domainConfig()
 servers=cmo.getServers()
+
+def updateHTTPlog(serverName):
+    """
+    Function to update the http log files for weblogic domain
+    """
+    print '======= Enabling webserver Log Retention for ' + serverName + '======='
+    cd('/Servers/'+serverName+'/WebServer/'+serverName+'/WebServerLog/'+serverName) # change directory to the servers webserver log path
+    cmo.setLoggingEnabled(true) # ENable logging
+    cmo.setNumberOfFilesLimited(true) # Enable the retention of log files.
+    cmo.setFileCount('100')  #number of http log files to be retained.
+
+def updateGeneralLog(serverName):
+    """
+    function to update the general logs.
+    """
+    print '======= Enabling Log Retention for ' + serverName + '======='
+    cd('/Servers/'+serverName+'/Log/'+serverName) # Change directory to weblogic server's general log location.
+    cmo.setLog4jLoggingEnabled(true)  # Enable logging
+    cmo.setNumberOfFilesLimited(true) # ENable the retention of log files
+    cmo.setFileCount('100')  #Set the number of files to be retained.
 
 for x in servers:
     try:
         edit()
         startedit()
         serverName = x.getName()
-        print '======= Enabling webserver Log Retention for ' + serverName + '======='
-        cd('/Servers/'+serverName+'/WebServer/'+serverName+'/WebServerLog/'+serverName)
-        cmo.setLoggingEnabled(true)
-        cmo.setNumberOfFilesLimited(true)
-        cmo.setFileCount('100')
-        cmo.FileMinSize('5000')
-        print '======= Enabling Log Retention for ' + serverName + '======='
-        cd('/Servers/'+serverName+'/Log/'+serverName)
-        cmo.setLog4jLoggingEnabled(true)
-        cmo.setNumberOfFilesLimited(true)
-        cmo.setFileCount('100')
-        cmo.FileMinSize('5000')
+        if serverName is not 'adminserver': # test to check if the serverName is not adminserver and then run the below commands.
+            updateGeneralLog(serverName)
+            updateHTTPlog(serverName)
         save()
         activate()
         print ' '
         print ' '
         ls()
-    except:
-        java.lang.Exception, ex:
+    except java.lang.Exception, ex:
         print 'Exception on Changing the Log Attributes: ' + ex.toString()
         save()
 
